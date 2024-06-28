f483332f585beb60800f558124c795ad59cb10ca
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-06-28 14:34:04 +0100

    Logo Screen,add assets ,animation logo,Theme

---
+++ lib/core/Theme/AppColor.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColor {
+  AppColor._();
+
+  static const Color textColor = Color(0xFF433728);
+  static const Color white= Color(0xFFFFFFFF);
+
+}
--- lib/core/Ui/Animation/Logoo.dart
+++ lib/core/Ui/Animation/Logoo.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class LogoP extends StatefulWidget {
@@ -10,6 +11,18 @@ class LogoP extends StatefulWidget {
 class _LogoPState extends State<LogoP> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: AnimatedSplashScreen(
+          splashTransition: SplashTransition.fadeTransition,
+          splash: Image.asset(
+            'assets/Logo/logo.jpg',
+            width: 300,
+            height: 300,
+          ),
+          nextScreen: const LogoP(),
+        ),
+      ),
+    );
   }
 }
