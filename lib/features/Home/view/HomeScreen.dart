5be6eea092c8c53fc0dba090bf954395421de36f
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-07 03:14:27 +0100

    wrap homeS with Backg

--- lib/features/Home/view/HomeScreen.dart
+++ lib/features/Home/view/HomeScreen.dart
@@ -1,5 +1,6 @@
 
 import 'package:book_store/core/Theme/AppColor.dart';
+import 'package:book_store/core/Ui/Widget/CustomBackgroundImage.dart';
 import 'package:flutter/material.dart';
 
 class HomeP extends StatefulWidget {
@@ -15,10 +16,12 @@ class _HomePState extends State<HomeP> {
     return const Scaffold(
 
       body:SingleChildScrollView(
-        child: Column(
-          children: [
-            Text('Hand-picked for you',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: 'IbarraRealNova',color: AppColor.textColor),)
-          ],
+        child: CustomBackGroundImg(
+          child: Column(
+            children: [
+              Text('Hand-picked for you',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontFamily: 'IbarraRealNova',color: AppColor.textColor),)
+            ],
+          ),
         ),
       )
 
