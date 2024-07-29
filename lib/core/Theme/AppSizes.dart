aa7fb7a9e25a5514b4b0f0a230bc78004c550fbe
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-29 19:36:47 +0100

    fix card overflow

--- lib/core/Theme/AppSizes.dart
+++ lib/core/Theme/AppSizes.dart
@@ -5,7 +5,7 @@ class AppSizes {
   static const double htext = 4;
   static const double BetweenButton = 80;
   static const double bar = 60;
-  static const double htC = 256;
+  static const double htC = 320;
   static const double wdC = 152;
   static const double gtext = 16;
   static const double hpic = 510;
